33ea89470dbde6e46a815c5fc6b353ce1f81a2b1
koreakimbing <99488368+koreakimbing@users.noreply.github.com>
2022-03-01 22:17:48 +0900

    Add files via upload

--- Untitled-1.c
+++ Untitled-1.c
@@ -1,2 +1,18 @@
-async def _ping(ctx):
-	await ctx.send("pong!")
+<!DOCTYPE html>
+<html>
+  <head>
+     <title>웹사이트 이름</title>
+     <style>
+       h1 {
+         color: red;
+       }
+       h2 {
+         color: blue;
+       }
+     </style>
+  </head>
+  <body>
+     <h1>타이틀</h1>
+     <h2>타이틀</h2>
+  </body>
+</html>
